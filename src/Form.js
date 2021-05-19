43193e3b6f3924127b24177f9222de1ea144593e
mayuripandey <mayuripandey4@gmail.com>
2021-05-19 10:28:30 +0200

    form signup complete

--- src/Form.js
+++ src/Form.js
@@ -1,22 +1,29 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import './Form.css';
 import FormSignup from './FormSignup';
 import FormSuccess from './FormSuccess';
 
 const Form = () => {
-    const [isSubmitted, setIsSubmitted] = useState(false)
-    
-    
-    function submitForm(){
-     setIsSubmitted(true);   
-    }
-    return (
-        <div>
-           
-            {!isSubmitted ? 
-            <FormSignup submitForm=
-            {submitForm} /> : <FormSuccess />}
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  function submitForm() {
+    setIsSubmitted(true);
+  }
+  return (
+    <>
+      <div className='form-container'>
+        <span className='close-btn'>×</span>
+        <div className='form-content-left'>
+          <img className='form-img' src='img/sw.PNG' alt='spaceship' />
         </div>
-    );
+        {!isSubmitted ? (
+          <FormSignup submitForm={submitForm} />
+        ) : (
+          <FormSuccess />
+        )}
+      </div>
+    </>
+  );
 };
 
 export default Form;
--- src/useForm.js
+++ src/useForm.js
@@ -1,30 +1,40 @@
-import {useState, useeffect} from 'react';
-const useForm = validate => {
-    const [values, setValues] = useState({
-        userName: '',
-        email: '',
-        password: '',
-        password2: ''
+import { useState, useEffect } from 'react';
 
+const useForm = (callback, validate) => {
+  const [values, setValues] = useState({
+    username: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+  const [errors, setErrors] = useState({});
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value
     });
-    const [errors, setErrors] = useState({})
-    const [isSubmitting, setIsSubmitting] = useState(false)
-    const handleChange = e => {
-        const {name, value} =  e.target
-        setValues({
-            ...values,
-            [name]: value
-        });
-    };
+  };
 
-    const handleSubmit = e => {
-        e.preventDefault();
+  const handleSubmit = e => {
+    e.preventDefault();
 
-        setErrors(validate(values));
-        setIsSubmitting(true);
-    };
+    setErrors(validate(values));
+    setIsSubmitting(true);
+  };
 
-    return {handleChange, values, handleSubmit, errors};
-}
+  useEffect(
+    () => {
+      if (Object.keys(errors).length === 0 && isSubmitting) {
+        callback();
+      }
+    },
+    [errors]
+  );
+
+  return { handleChange, handleSubmit, values, errors };
+};
 
 export default useForm;
